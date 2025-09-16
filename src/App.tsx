513d59373e2a7563c8ee8090879ae8c5c452ea96
kaka_256 <kumarai460@gmail.com>
2025-09-16 11:15:32 +0900

    fix: Add Prefecture request Insert prefecture name for graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,63 @@
-import { useEffect } from "react";
-import Highcharts from "highcharts";
+import { useEffect, useState } from "react";
+import Highcharts, { type SeriesOptionsType } from "highcharts";
+
+interface PrefecturesResponse {
+  message: string | null;
+  result: Prefecture[];
+}
+
+interface Prefecture {
+  prefCode: number;
+  prefName: string;
+}
 
 function App() {
+  const [prefectures, setPrefectures] = useState<Prefecture[] | null>(null);
+  const [graphLines, setGraphLines] = useState<SeriesOptionsType[] | undefined>(
+    undefined,
+  );
+
+  async function apiRequest() {
+    const prefResponse = await fetch("/api/v1/prefectures");
+    const prefData: PrefecturesResponse = await prefResponse.json();
+    setPrefectures(prefData.result);
+  }
+
   useEffect(() => {
+    try {
+      apiRequest();
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!prefectures || prefectures.length === 0) {
+      return;
+    }
+
+    setGraphLines(
+      prefectures.map((prefecture) => {
+        return {
+          type: "line",
+          name: prefecture.prefName,
+          data: [1, 4, 1, 4, 2],
+        };
+      }),
+    );
+  }, [prefectures]);
+
+  useEffect(() => {
+    if (!graphLines) {
+      return;
+    }
+
     const mychart = new Highcharts.Chart({
       chart: { renderTo: "hchart" },
       title: { text: "タイトル" },
       xAxis: { title: { text: "横軸" } },
       yAxis: { title: { text: "縦軸" } },
-      series: [
-        { type: "line", name: "ライン1", data: [1, 4, 1, 4, 2] },
-        { type: "line", name: "ライン2", data: [1, 7, 3, 2, 0] },
-      ],
+      series: graphLines,
       legend: {
         layout: "vertical",
         align: "right",
@@ -21,7 +67,7 @@ function App() {
     return () => {
       mychart.destroy();
     };
-  }, []);
+  }, [graphLines]);
 
   return (
     <>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
+export default defineConfig(() => {
+  const env = loadEnv(process.env.NODE_ENV as string, process.cwd(), "VITE_");
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          headers: {
+            "X-API-KEY": env.VITE_XAPI_KEY,
+          },
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    plugins: [tailwindcss(), react()],
+  };
 });
