bbba1693d2a5ea9609bcd96a8acf5699c124ff13
kaka_256 <kumarai460@gmail.com>
2025-10-04 18:01:36 +0900

    fix: api function

--- functions/api.js
+++ functions/api.js
@@ -1,25 +1,24 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
+export default async function onRequest(context) {
+  const { request, env } = context;
+  const url = new URL(request.url);
 
-    if (url.pathname.startsWith("/api")) {
-      url.pathname = url.pathname.replace("/api", "");
+  if (url.pathname.startsWith("/api")) {
+    url.pathname = url.pathname.replace("/api", "");
 
-      const targetUrl = new URL(url.pathname + url.search, env.VITE_API_URL);
+    const targetUrl = new URL(url.pathname + url.search, env.VITE_API_URL);
 
-      const newRequest = new Request(targetUrl.toString(), {
-        method: request.method,
-        headers: {
-          ...Object.fromEntries(request.headers),
-          "X-API-KEY": env.VITE_XAPI_KEY,
-        },
-        body: request.body,
-        redirect: "follow",
-      });
+    const newRequest = new Request(targetUrl.toString(), {
+      method: request.method,
+      headers: {
+        ...Object.fromEntries(request.headers),
+        "X-API-KEY": env.VITE_XAPI_KEY,
+      },
+      body: request.body,
+      redirect: "follow",
+    });
 
-      return fetch(newRequest);
-    }
+    return fetch(newRequest);
+  }
 
-    return new Response("Not found", { status: 404 });
-  },
-};
+  return new Response("Not found", { status: 404 });
+}
